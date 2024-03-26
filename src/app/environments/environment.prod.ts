ff8a2f78d9fb5db12d7d61de77d8f931b2785d52
Athul030 <techmobo2@gmail.com>
2024-03-26 09:45:41 +0530

    env added back

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    apiUrl: 'http://localhost:8080', 
+    apiUrlWS: 'ws://localhost:8080', 
+    appId:1236957569,
+    serverSecret:'090e9d8fa0ceae88e933f8512988ac05',
+    channel: 'memegram',
+    token : 
+    '007eJxTYIjt+Mb91iHXpHvph7b7rFmLQ2SlXBtzLk9TzXyb/nV5nb8CQ7JBWpKlqVFSoqmFoUlaommSZUqSuVGSsUmihamhqaXhKsb3qQ2BjAxi2ZcYGKEQxOdgyE3NTU0vSsxlYAAA28chBw=='
+
+  };
+  

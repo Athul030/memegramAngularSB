3670ddae0f2510b03b55520daaf8f778a0283659
Athul030 <techmobo2@gmail.com>
2024-03-26 10:20:58 +0530

    changed url to aws

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,7 +1,11 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080',
-    apiUrlWS: 'ws://localhost:8080', 
+    // apiUrl: 'http://localhost:8080',
+    apiUrl: 'http://35.175.236.172:8080',
+
+    // apiUrlWS: 'ws://localhost:8080',
+    apiUrlWS: 'ws://35.175.236.172:8080', 
+
     appId:1236957569,
     serverSecret:'090e9d8fa0ceae88e933f8512988ac05',
     channel: 'memegram',
